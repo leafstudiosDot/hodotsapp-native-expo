186e38f94d1e0b876df4ed5b787cc0c9df0d803a
alia manuba <tux.penguin.tab@gmail.com>
2021-04-09 13:18:26 +0800

    Updates and Changes

---
+++ pages/posts/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+
+class Posts extends Component {
+
+}
+
+export default Posts;
---
+++ pages/user/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Text, View, ScrollView } from 'react-native';
+
+class User extends Component {
+
+}
+
+export default User;
