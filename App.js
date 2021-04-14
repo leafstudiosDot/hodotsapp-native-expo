32c9d19a5d4a5f55abd939443f849681ec9873e3
alia manuba <tux.penguin.tab@gmail.com>
2021-04-14 14:27:34 +0800

    Added some splash for Android

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Platform, AppRegistry } from 'react-native';
 import Main from './Main'
 
+/*async _cacheResourcesAsync() {
+  return new Promise(async(resolve) => {
+    try {
+      await F
+    } catch(error) {
+      console.error(error)
+    }
+  })
+}*/
+
 export default function App() {
   return (
     <View style={styles.container}>
